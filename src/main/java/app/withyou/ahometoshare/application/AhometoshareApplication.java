bcda4cdbc165f2ed819e9ed95d9c7f599641b33f
praylee <prayleee@gmail.com>
2019-02-02 23:02:34 -0500

    initial commit

---
+++ src/main/java/app/withyou/ahometoshare/application/AhometoshareApplication.java
@@ -0,0 +1,14 @@
+package app.withyou.ahometoshare.application;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AhometoshareApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AhometoshareApplication.class, args);
+	}
+
+}
+
